9fc6765e148fa741878b32ec72131d001894818e
Felipe A. Costa <felipeagc2@gmail.com>
2023-12-23 17:04:42 -0300

    Add Schlick approximation (looks good now)

--- src/material.hpp
+++ src/material.hpp
@@ -58,6 +58,7 @@ struct MaterialDielectric {
         sycl::float4 &attenuation
     ) const {
         attenuation = sycl::float4(1, 1, 1, 1);
+
         bool front_face = dot(dir, outward_normal) < 0;
 
         sycl::float3 normal = front_face ? outward_normal : -outward_normal;
@@ -65,17 +66,25 @@ struct MaterialDielectric {
 
         sycl::float3 unit_direction = normalize(dir);
         float cos_theta = fminf(dot(-unit_direction, normal), 1.0f);
-        float sin_theta = sycl::sqrt(1.0f - cos_theta*cos_theta);
+        float sin_theta = sycl::sqrt(1.0f - cos_theta * cos_theta);
 
         bool cannot_refract = refraction_ratio * sin_theta > 1.0f;
 
-        if (cannot_refract) {
+        if (cannot_refract ||
+            reflectance(cos_theta, refraction_ratio) > rng(0.0f, 1.0f)) {
             dir = reflect(unit_direction, normal);
         } else {
             dir = refract(unit_direction, normal, refraction_ratio);
         }
 
         return true;
+    }
+
+    static float reflectance(float cosine, float ref_idx) {
+        // Use Schlick's approximation for reflectance.
+        float r0 = (1.0f - ref_idx) / (1.0f + ref_idx);
+        r0 = r0 * r0;
+        return r0 + (1.0f - r0) * sycl::pow((1.0f - cosine), 5.0f);
     }
 };
 
