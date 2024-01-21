0caa6f1e54f096a603343c8299fb5c5de0265fee
Felipe A. Costa <felipeagc2@gmail.com>
2024-01-21 00:47:10 -0300

    Add benchmark script and results

---
+++ benchmark.py
@@ -0,0 +1,48 @@
+import os
+import subprocess
+import re
+import itertools
+
+depth_samples = [
+    (10, 32),
+    (50, 32),
+    (50, 32),
+    (100, 32),
+
+    (10, 256),
+    (10, 512),
+]
+scenes = ['./assets/sponza.glb', './assets/minecraft.glb']
+renderers = ['-m', '-w']
+
+with open('benchmark.csv', 'w') as f:
+    f.write("renderer,depth,samples,scene,time,rays_per_sec\n")
+
+combinations = itertools.product(scenes, depth_samples, renderers)
+for (scene, (depth, samples), renderer) in combinations:
+    print(f"Running benchmark for {renderer} with {samples} samples and {depth} depth")
+
+    for i in range(6):
+        print(f"Iteration {i}")
+        output = subprocess.check_output([
+            "./build/raytracer",
+            renderer,
+            "-d", str(depth),
+            "-s", str(samples),
+            scene
+        ])
+        if i == 0:
+            continue
+
+        output = output.decode("utf-8")
+
+        m = re.search(r'Rays/sec: (\d+\.\d+)M', output)
+        rays_per_sec = float(m.group(1))
+
+        m = re.search(r'Time measured: (\d+\.\d+) seconds', output)
+        time = float(m.group(1))
+
+        print(f"({renderer}, {time}, {rays_per_sec})")
+
+        with open('benchmark.csv', 'a') as f:
+            f.write(f"{renderer},{depth},{samples},{scene},{time},{rays_per_sec}\n")
