595956bd85c6905a54c57f6cbd2acdc78780ab7d
Felipe A. Costa <felipeagc2@gmail.com>
2024-01-28 17:14:24 -0300

    Add total rays to benchmark script

--- benchmark.py
+++ benchmark.py
@@ -6,17 +6,16 @@ import itertools
 depth_samples = [
     (10, 32),
     (50, 32),
-    (50, 32),
     (100, 32),
 
-    (10, 256),
-    (10, 512),
+    (10, 64),
+    (10, 128),
 ]
 scenes = ['./assets/sponza.glb', './assets/minecraft.glb']
 renderers = ['-m', '-w']
 
 with open('benchmark.csv', 'w') as f:
-    f.write("renderer,depth,samples,scene,time,rays_per_sec\n")
+    f.write("renderer,depth,samples,scene,time,rays_per_sec,ray_count\n")
 
 combinations = itertools.product(scenes, depth_samples, renderers)
 for (scene, (depth, samples), renderer) in combinations:
@@ -42,7 +41,10 @@ for (scene, (depth, samples), renderer) in combinations:
         m = re.search(r'Time measured: (\d+\.\d+) seconds', output)
         time = float(m.group(1))
 
-        print(f"({renderer}, {time}, {rays_per_sec})")
+        m = re.search(r'Total rays: (\d+)', output)
+        ray_count = int(m.group(1))
+
+        print(f"({renderer}, {time}, {rays_per_sec}, {ray_count})")
 
         with open('benchmark.csv', 'a') as f:
-            f.write(f"{renderer},{depth},{samples},{scene},{time},{rays_per_sec}\n")
+            f.write(f"{renderer},{depth},{samples},{scene},{time},{rays_per_sec},{ray_count}\n")
